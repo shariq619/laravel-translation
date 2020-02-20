824571d3b48e3fbe0d8fdd161246a146aec5d2a1
Muhammad Shariq Ali <muhammadshariqali@gmail.com>
2020-02-20 12:30:46 +0500

    Update Translation.php

--- src/Translation.php
+++ src/Translation.php
@@ -8,6 +8,25 @@ class Translation extends Model
 {
     protected $guarded = [];
 
+    protected $maps = [
+        'language_pack_id' => 'language_id',
+        'target'            => 'group'
+    ];
+
+    protected $append = ['language_id','group'];
+
+    public function getLanguageIdAttribute()
+    {
+        return $this->attributes['language_pack_id'];
+    }
+
+    public function getGroupAttribute()
+    {
+        return $this->attributes['target'];
+    }
+
+    protected $hidden = ['language_pack_id','target'];
+
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
@@ -17,16 +36,16 @@ class Translation extends Model
 
     public function language()
     {
-        return $this->belongsTo(Language::class);
+        return $this->belongsTo(Language::class,'language_pack_id');
     }
 
     public static function getGroupsForLanguage($language)
     {
         return static::whereHas('language', function ($q) use ($language) {
-            $q->where('language', $language);
-        })->whereNotNull('group')
-            ->where('group', 'not like', '%single')
-            ->select('group')
+            $q->where('name', $language);
+        })->whereNotNull('target')
+            ->where('target', 'not like', '%single')
+            ->select('target')
             ->distinct()
             ->get();
     }
